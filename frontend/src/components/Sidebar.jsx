789bab226485de1f69f7c963145ba0474b5fea23
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2024-11-03 23:41:54 +0530

    13th update  inline style

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Sidebar() {
     return (
     <div>
-        <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{ width: "280px" }}>
+        <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark flex-shrinkVercel" >
                     <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
                         <span className="fs-4">User Dasdboard</span>
